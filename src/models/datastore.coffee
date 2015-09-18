db5493b370845f139f1eb64bfbf376c712bebd0b
sqrt <sqrt@octoblu.com>
2015-09-18 14:27:57 -0700

    Boot redis using env creds, v1.1.0

--- src/models/datastore.coffee
+++ src/models/datastore.coffee
@@ -3,7 +3,7 @@ redis = require 'redis'
 class Datastore
   constructor: (dependencies={})->
     {@client} = dependencies
-    @client ?= redis.createClient()
+    @client ?= redis.createClient process.env.REDIS_PORT, process.env.REDIS_HOST, auth_pass: process.env.REDIS_PASSWORD
 
   get: (key, callback=->) =>
     @client.get key, (error, data) =>
--- start-flow.sh
+++ start-flow.sh
@@ -6,5 +6,6 @@ UUID=dd3d787a-7833-4581-9287-3ad2c5a1273a
 INSTANCE_UUID=05093590-5e37-11e5-8326-0101d2a7f1f8
 
 DATA='{"devices": ["dd3d787a-7833-4581-9287-3ad2c5a1273a"], "topic": "button", "payload": {"from": "abfab830-5d9a-11e5-a513-13ce059e4cb9", "foo": "bar"}}'
-URL="http://localhost:5050/flows/$UUID/instances/$INSTANCE_UUID/messages"
+URL="http://nanocyte-engine.octoblu.com/flows/$UUID/instances/$INSTANCE_UUID/messages"
+#URL="http://localhost:5050/flows/$UUID/instances/$INSTANCE_UUID/messages"
 curl -X POST -H 'Content-Type: application/json' -d "$DATA" "$URL"
