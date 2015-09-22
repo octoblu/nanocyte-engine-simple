034d5abea7732d4bb7c6c545a349293a86f94fea
sqrt <sqrt@octoblu.com>
2015-09-22 08:54:30 -0700

    Comment out christacheio and dynamicNode

--- src/stash/dynamicNode.js
+++ src/stash/dynamicNode.js
@@ -1,20 +1,20 @@
-'use strict';
-var christacheio = require('./christacheio');
-var debug = require('debug')('flow-runner:dynamicNode');
-
-var escapeDoubleQuote = function(data) {
-  if(!data) {
-     return data;
-  }
-  return data.toString().replace(/"/g, '\\"');
-}
-
-var dynamicNode = function(node, msg){
-  var nodeString = christacheio(['"{{', '}}"'], JSON.stringify(node), {msg: msg}, JSON.stringify);
-  debug('first pass: ', nodeString);
-  nodeString = christacheio(['{{', '}}'], nodeString, {msg: msg}, escapeDoubleQuote); // don't stringify the second time
-  debug('got nodeString', nodeString);
-  return JSON.parse(nodeString);
-};
-
-module.exports = dynamicNode;
+// 'use strict';
+// var christacheio = require('./christacheio');
+// var debug = require('debug')('flow-runner:dynamicNode');
+//
+// var escapeDoubleQuote = function(data) {
+//   if(!data) {
+//      return data;
+//   }
+//   return data.toString().replace(/"/g, '\\"');
+// }
+//
+// var dynamicNode = function(node, msg){
+//   var nodeString = christacheio(['"{{', '}}"'], JSON.stringify(node), {msg: msg}, JSON.stringify);
+//   debug('first pass: ', nodeString);
+//   nodeString = christacheio(['{{', '}}'], nodeString, {msg: msg}, escapeDoubleQuote); // don't stringify the second time
+//   debug('got nodeString', nodeString);
+//   return JSON.parse(nodeString);
+// };
+//
+// module.exports = dynamicNode;
