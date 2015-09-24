a4b55eabcf708867a5730f9d529cd6d5dbee5ee6
sqrt <sqrt@octoblu.com>
2015-09-24 10:34:50 -0700

    Add cyclomatic complexity

--- click-trigger.sh
+++ click-trigger.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # /flows//instances/
-#URL="http://nanocyte-engine.octoblu.com/flows/$FLOW_UUID/instances/$INSTANCE_UUID/messages"
 
+HOST="https://nanocyte-engine.octoblu.com"
+#HOST="http://localhost:5050"
 
 FLOW_UUID=c36f335a-d820-42bc-bedb-b08775931318
-INSTANCE_UUID=338b79a0-475c-45dd-a4d7-334e866e9911
+INSTANCE_UUID=abaa99d0-081f-4967-a8fe-dada9e0c6754
 TRIGGER_UUID=9f7242a0-621c-11e5-b85f-0b844c991eb6
 
-URL="http://localhost:5050/flows/$FLOW_UUID/instances/$INSTANCE_UUID/messages"
+URL="$HOST/flows/$FLOW_UUID/instances/$INSTANCE_UUID/messages"
 DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": [1,2,3]}}'
 curl -X POST -H 'Content-Type: application/json' -d "$DATA" "$URL"
