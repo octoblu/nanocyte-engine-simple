ef44641229a6e7474d95eafe8d0875ae3838eadc
redaphid <a@octoblu.com>
2015-10-07 16:41:13 -0700

    v1.13.67

--- click-trigger.sh
+++ click-trigger.sh
@@ -9,8 +9,8 @@ HOST="localhost:5050"
 
 FLOW_UUID=bf3fbc43-c90b-4e0d-a609-6f58caed29d9
 FLOW_TOKEN=5a1f5d795cde22c982d61655d2d59a44b0495c2c
-INSTANCE_UUID=f332e9d7-7f98-4f8e-89e1-6f069d8e8572
-TRIGGER_UUID=f84bd7f0-6bb9-11e5-ae86-6b89656e6b1a
+INSTANCE_UUID=a1c06b97-9a3b-4485-8492-a2b91166cd6c
+TRIGGER_UUID=76c69c10-6853-11e5-9032-27d0368312f4
 
 URL="${PROTOCOL}${FLOW_UUID}:${FLOW_TOKEN}@${HOST}/flows/${FLOW_UUID}/instances/${INSTANCE_UUID}/messages"
 DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": [1,2,3]}}'
