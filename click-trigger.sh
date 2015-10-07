a73f17b9b9837cc0f04e448cf7bd3e5f6a819ec8
sqrt <sqrt@octoblu.com>
2015-10-06 17:03:18 -0700

    v1.13.65 Detect and kill infinite flow loops

--- click-trigger.sh
+++ click-trigger.sh
@@ -9,8 +9,8 @@ HOST="localhost:5050"
 
 FLOW_UUID=bf3fbc43-c90b-4e0d-a609-6f58caed29d9
 FLOW_TOKEN=5a1f5d795cde22c982d61655d2d59a44b0495c2c
-INSTANCE_UUID=0cdda898-c35a-4424-ac66-b521f357ea08
-TRIGGER_UUID=7ff69550-6c78-11e5-916c-43f7bf0bd17c
+INSTANCE_UUID=f332e9d7-7f98-4f8e-89e1-6f069d8e8572
+TRIGGER_UUID=f84bd7f0-6bb9-11e5-ae86-6b89656e6b1a
 
 URL="${PROTOCOL}${FLOW_UUID}:${FLOW_TOKEN}@${HOST}/flows/${FLOW_UUID}/instances/${INSTANCE_UUID}/messages"
 DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": [1,2,3]}}'
