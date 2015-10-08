b2efce246ded184bc1454264a4ae7b0f59ed7242
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-10-08 16:24:44 -0700

    v1.14.4 allow click-trigger to get the instanceid from the command line

--- click-trigger.sh
+++ click-trigger.sh
@@ -9,8 +9,8 @@ HOST="localhost:5050"
 
 FLOW_UUID=bf3fbc43-c90b-4e0d-a609-6f58caed29d9
 FLOW_TOKEN=5a1f5d795cde22c982d61655d2d59a44b0495c2c
-INSTANCE_UUID=a1c06b97-9a3b-4485-8492-a2b91166cd6c
-TRIGGER_UUID=76c69c10-6853-11e5-9032-27d0368312f4
+INSTANCE_UUID=$1
+TRIGGER_UUID=59d37a50-6e0d-11e5-baca-ed7f24b332b1
 
 URL="${PROTOCOL}${FLOW_UUID}:${FLOW_TOKEN}@${HOST}/flows/${FLOW_UUID}/instances/${INSTANCE_UUID}/messages"
 DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": [1,2,3]}}'
