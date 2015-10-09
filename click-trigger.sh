094f388a058b32641604a3fea67f795adc1490df
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-10-09 15:25:08 -0700

    v1.14.12 github flow

--- click-trigger.sh
+++ click-trigger.sh
@@ -7,11 +7,11 @@ PROTOCOL="http://"
 HOST="localhost:5050"
 # HOST="172.17.8.101:5050"
 
-FLOW_UUID=bf3fbc43-c90b-4e0d-a609-6f58caed29d9
-FLOW_TOKEN=5a1f5d795cde22c982d61655d2d59a44b0495c2c
+FLOW_UUID=a2d0ba1a-a0f3-4f2c-9062-14cd3e9c5ffe
+FLOW_TOKEN=58254f979311c4fb27bbb579751cd6863695573e
 INSTANCE_UUID=$1
-TRIGGER_UUID=59d37a50-6e0d-11e5-baca-ed7f24b332b1
+TRIGGER_UUID=2303e740-6dfd-11e5-bde0-6bdc4f7956bb
 
 URL="${PROTOCOL}${FLOW_UUID}:${FLOW_TOKEN}@${HOST}/flows/${FLOW_UUID}/instances/${INSTANCE_UUID}/messages"
-DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": [1,2,3]}}'
+DATA='{"devices": ["'$FLOW_UUID'"], "topic": "button", "payload": {"from": "'$TRIGGER_UUID'", "foo": "bar"}}'
 curl -X POST -H 'Content-Type: application/json' -d "$DATA" "$URL"
